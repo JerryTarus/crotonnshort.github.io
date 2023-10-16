04babd78417c34d0b28ce2c7f6e6a7ca2572b055
Jerry Tarus <jerrytarus@gmail.com>
2023-10-16 17:26:57 +0300

    Added the a function to shorten URLs

--- src/crotonn.js
+++ src/crotonn.js
@@ -41,3 +41,12 @@ document.addEventListener('DOMContentLoaded', function () {
         originalUrlInput.value = '';
         generatedUrlDiv.style.display = 'none';
     });
+// Time to write function to shorten the URL
+function shortenURL() {
+    const originalUrl = originalUrlInput.value;
+
+    // Bitly API Key for our app (bitly calls it access token, same thing)
+    const apiKey = '6fab06a885c7e3b4c88f9aa919496dcfa8f6976a';
+
+    // This clears any previous error messages
+    generatedUrlDiv.classList.remove('error');
