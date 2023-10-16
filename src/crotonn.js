bcf24bfc352911c48ad2261886d9fc5d27c6fb42
Jerry Tarus <jerrytarus@gmail.com>
2023-10-16 17:18:47 +0300

    Set up event listeners on JS

---
+++ src/crotonn.js
@@ -0,0 +1,12 @@
+// JavaScript code to improve site functionalities
+
+document.addEventListener('DOMContentLoaded', function () {
+    const shortenButton = document.getElementById('shorten-button');
+    const originalUrlInput = document.getElementById('original-url');
+    const generatedUrlDiv = document.getElementById('generated-url');
+    const shortLink = document.getElementById('short-link');
+
+     // This event listener helps to shorten URL
+     shortenButton.addEventListener('click', () => {
+        shortenURL();
+});
