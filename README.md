2a4c89dc50870f27f945d2a3ac8cde8893bdd65c
Jerry Tarus <jerrytarus@gmail.com>
2023-10-16 17:34:21 +0300

    Cross Checked code for any errors then started updating readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # crotonnshort.github.io
+
+# Crotonn Short 
+
+Crotonn Short is a simple and user-friendly web app that allows you to shorten long URLs with ease. It is built using HTML, CSS, and JavaScript and leverages the Bitly API for URL shortening.
+
+![Alt text](img/crotonnshort.png)
+
+## Table of Contents
+
+- [Features](#features)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Usage](#usage)
+- [API Integration](#api-integration)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- Shorten long URLs quickly and efficiently.
+- User-friendly interface with a clean design.
+- Asynchronous interaction with the Bitly API.
+- Error handling for invalid URLs and API errors.
+- Single-page web app with no page redirects.
+- Utilizes event listeners for seamless user interaction.
+
+## Getting Started
+
+Follow these instructions to get your copy of Crotonn Short up and running.
+
+### Prerequisites
+
+Before you begin, you'll need the following:
+
+- A Bitly account and API key (access token). You can obtain one by following Bitly's registration process.
+
+### Installation
+
+1. Clone the repository to your local machine:
+
+
+git clone https://github.com/your-username/crotonn-short.git
--- src/crotonn.js
+++ src/crotonn.js
@@ -6,11 +6,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const generatedUrlDiv = document.getElementById('generated-url');
     const shortLink = document.getElementById('short-link');
 
-     // This event listener helps to shorten URL
-     shortenButton.addEventListener('click', () => {
+    // This event listener helps to shorten URL
+    shortenButton.addEventListener('click', () => {
         shortenURL();
-});
-
+    });
 
     // Event Listener to listen to submit button on Enter key press
     originalUrlInput.addEventListener('keydown', (e) => {
@@ -20,9 +19,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-
-       // This focuses on input when the page loads
-       document.addEventListener('DOMContentLoaded', () => {
+    // This focuses on input when the page loads
+    document.addEventListener('DOMContentLoaded', () => {
         originalUrlInput.focus();
     });
 
@@ -41,45 +39,45 @@ document.addEventListener('DOMContentLoaded', function () {
         originalUrlInput.value = '';
         generatedUrlDiv.style.display = 'none';
     });
-// Time to write function to shorten the URL
-function shortenURL() {
-    const originalUrl = originalUrlInput.value;
 
-    // Bitly API Key for our app (bitly calls it access token, same thing)
-    const apiKey = '6fab06a885c7e3b4c88f9aa919496dcfa8f6976a';
+    // Time to write function to shorten the URL
+    function shortenURL() {
+        const originalUrl = originalUrlInput.value;
 
-    // This clears any previous error messages
-    generatedUrlDiv.classList.remove('error');
+        // Bitly API Key for our app (bitly calls it access token, same thing)
+        const apiKey = '6fab06a885c7e3b4c88f9aa919496dcfa8f6976a';
 
-    // Fetch API to make an asynchronous POST request to the Bitly API
-    fetch(`https://api-ssl.bitly.com/v4/shorten`, {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${apiKey}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            long_url: originalUrl,
-        }),
-    })
+        // This clears any previous error messages
+        generatedUrlDiv.classList.remove('error');
 
-    .then(response => response.json())
-    .then(data => {
-        if (data.link) {
-            // This block will help display the shortened URL
-            shortLink.href = data.link;
-            shortLink.textContent = data.link;
-            generatedUrlDiv.style.display = 'block';
-        } else {
-            // While this handles cases where the response is not a valid URL
-            generatedUrlDiv.classList.add('error');
-            shortLink.textContent = 'Invalid URL or API error';
-            generatedUrlDiv.style.display = 'block';
-        }
-    })
+        // Fetch API to make an asynchronous POST request to the Bitly API
+        fetch(`https://api-ssl.bitly.com/v4/shorten`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${apiKey}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                long_url: originalUrl,
+            }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.link) {
+                // This block will help display the shortened URL
+                shortLink.href = data.link;
+                shortLink.textContent = data.link;
+                generatedUrlDiv.style.display = 'block';
+            } else {
+                // While this handles cases where the response is not a valid URL
+                generatedUrlDiv.classList.add('error');
+                shortLink.textContent = 'Invalid URL or API error';
+                generatedUrlDiv.style.display = 'block';
+            }
+        })
 
-           // Error handling
-           .catch(error => {
+        // Error handling
+        .catch(error => {
             console.error('Error generating shortened URL:', error);
             generatedUrlDiv.classList.add('error');
             shortLink.textContent = 'Error generating shortened URL';
@@ -87,4 +85,3 @@ function shortenURL() {
         });
     }
 });
- 
